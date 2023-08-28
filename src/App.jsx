be5174395c8eb75091ae189172f672f9b6cb10ac
rtio <meneses.tio@gmail.com>
2023-08-28 18:19:25 -0300

    Add products const

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
+
+const PRODUCTS = [
+  {
+    id: 1,
+    name: 'Camiseta Básica',
+    price: '$35',
+    color: 'Preta',
+    image: {
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      alt: 'Camiseta preta básica com gola redonda e manga curta.',
+    },
+  },
+  {
+    id: 2,
+    name: 'Camiseta Básica',
+    price: '$40',
+    color: 'Branco Aspen',
+    image: {
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg',
+      alt: 'Camiseta branca básica com gola redonda e manga curta.',
+    },
+  },
+  {
+    id: 3,
+    name: 'Camiseta Básica',
+    price: '$39',
+    color: 'Carvão',
+    image: {
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg',
+      alt: 'Camiseta cinza básica com gola redonda e manga curta.',
+    },
+  },
+  {
+    id: 4,
+    name: 'Camiseta Básica',
+    price: '$35',
+    color: 'Pontos',
+    image: {
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg',
+      alt: 'Camiseta branca básica com gola redonda e manga curta.',
+    },
+  },
+]
+
 function App() {
   return (
     <div className="min-h-screen bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-yellow-600 via-purple-200 to-teal-200">
